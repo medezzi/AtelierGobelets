462bc66c356910c257770be01ab99c4d7f7eb841
medezzi <madou_zz@yahoo.fr>
2018-10-05 11:51:04 +0100

    add slide swip

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 AOS.init({
   duration: 2500,
 })
+
+
+function onchangeoption() {
+  console.log("bb")
+  /*var index = selectObj.selectedIndex;
+
+  //var name = document.getElementById("typegobelets").value;
+  console.log(index)
+
+  if (index == 0) {
+    document.getElementById("quantite").style.display = "block";
+    document.getElementById("quantite2").style.display = "none";
+
+  }
+  else {
+    document.getElementById("quantite").style.display = "none";
+    document.getElementById("quantite2").style.display = "block";
+  }
+*/
+
+}
+
+
